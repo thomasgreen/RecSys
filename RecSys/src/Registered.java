8c5dec6907b60392620649a50a7704007e29e8ab
Thomas Green <tomgreen135@gmail.com>
2017-01-31 11:14:48 +0000

    Additional User Classes

---
+++ RecSys/src/Registered.java
@@ -0,0 +1,31 @@
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Registered {
+
+	@SerializedName("#text")
+	@Expose
+	private Integer text;
+	
+	@SerializedName("unixtime")
+	@Expose
+	private String unixtime;
+
+	public Integer getText() {
+		return text;
+	}
+
+	public void setText(Integer text) {
+		this.text = text;
+	}
+
+	public String getUnixtime() {
+		return unixtime;
+	}
+
+	public void setUnixtime(String unixtime) {
+		this.unixtime = unixtime;
+	}
+
+}
