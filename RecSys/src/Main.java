d5a997b1b68b09632dc2fc26eff2c9604729e238
Thomas Green <tomgreen135@gmail.com>
2017-02-09 11:33:28 +0000

    Write found user names to txt file. Currently working on finding duplicates and stopping them from being written

--- RecSys/src/Main.java
+++ RecSys/src/Main.java
@@ -1,153 +1,135 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.TypeAdapter;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonToken;
-import com.google.gson.stream.JsonWriter;
-
-import FriendsPOJO.Friends;
 import FriendsPOJO.GetFriends;
-import UserPOJO.User;
 import UserPOJO.UserInfo;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.lang.reflect.Type;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
-import org.json.*;
 
 public class Main {
 
 	static String API_KEY = "78cc84967dc3a5fb577941e75bf7f8a9";
 	static Gson gson = new Gson();
 	private static String username;
-	
-	
+
 	public static void getUserInfo(String username) throws IOException {
-		
-		
-		final URL reqURL = new URL("http://ws.audioscrobbler.com/2.0/?method=user.getinfo&" +
-				"user=" + username + 
-				"&api_key=" + API_KEY + 
-				"&format=json");
-		
+
+		final URL reqURL = new URL("http://ws.audioscrobbler.com/2.0/?method=user.getinfo&" + "user=" + username
+				+ "&api_key=" + API_KEY + "&format=json");
+
 		final InputStream inputstream = APISend(reqURL);
-			
+
 		BufferedReader reader = new BufferedReader(new InputStreamReader(inputstream));
-		
-			
+
 		UserInfo userinfo = gson.fromJson(reader, UserInfo.class);
-		
-		if(userinfo != null){
-			
-			System.out.println("User Name: " + userinfo.getUser().getName() + 
-					"\nURL: " + userinfo.getUser().getUrl() +
-					"\nPlay Count: " + userinfo.getUser().getPlaycount()
-					);
+
+		if (userinfo != null) {
+
+			System.out.println("User Name: " + userinfo.getUser().getName() + "\nURL: " + userinfo.getUser().getUrl()
+					+ "\nPlay Count: " + userinfo.getUser().getPlaycount());
 		}
-		
+
 	}
-	
-	
-	public static void getUserFriends(String username) throws IOException{
-		
-		
-		final URL reqURL = new URL("http://ws.audioscrobbler.com/2.0/?method=user.getfriends&" +
-				"user=" + username + 
-				"&api_key=" + API_KEY + 
-				"&format=json");
-		
+
+	public static void getUserFriends(String username) throws IOException {
+
+		final URL reqURL = new URL("http://ws.audioscrobbler.com/2.0/?method=user.getfriends&" + "user=" + username
+				+ "&api_key=" + API_KEY + "&format=json");
+
 		final InputStream inputstream = APISend(reqURL);
-			
+
 		BufferedReader reader = new BufferedReader(new InputStreamReader(inputstream));
-		
-		
+
 		GetFriends getfriends = gson.fromJson(reader, GetFriends.class);
 		
+		//if(getfriends.getFriends() == null) throw new IllegalArgumentException();
 		
-		
-		
+
 		System.out.println("\nList of Friends: ");
+
 		
-		
-		//System.out.println(getfriends.getFriends().getUser().get(0).getName());
-		
-		
-		
-		int friendsTotal = getfriends.getFriends().getUser().size();
-		
-		for(int i=0; i<friendsTotal; i++){
-			System.out.println(getfriends.getFriends().getUser().get(i).getName());
-        }
-		
-		
-		//saving usernames to files
-		
-		
-		BufferedWriter writer = new BufferedWriter(new FileWriter("rsc/test1.txt", true));
-		
-	
-		for(int i=0; i<friendsTotal; i++){
-			writer.write(getfriends.getFriends().getUser().get(i).getName());
-			writer.newLine();
-        }
-		
-		
-		
-		
-		writer.flush();
-		writer.close();
-		
-	
-		
-		
+		try {
+			int friendsTotal = getfriends.getFriends().getUser().size();
+
+
+			// saving usernames to files
+
+			BufferedWriter writer = new BufferedWriter(new FileWriter("rsc/test1.txt", true));
+
+			for (int i = 0; i < friendsTotal; i++) {
+				String userToAdd = getfriends.getFriends().getUser().get(i).getName();
+				
+				Scanner filescanner = new Scanner("rsc.test1.txt");
+					
+			
+				
+				while(filescanner.hasNextLine()){
+				     if(userToAdd.equals(filescanner.nextLine().trim())){
+				        // found
+				        break;
+				      }else{
+				       // not found
+				    	writer.write(getfriends.getFriends().getUser().get(i).getName());
+						writer.newLine();
+				    	  
+
+				      }
+
+				 }
+				
+				
+				
+
+				//writer.write(getfriends.getFriends().getUser().get(i).getName());
+				//writer.newLine();
+				
+				filescanner.close();
+
+			}
+
+			writer.flush();
+			writer.close();
+			
+		} catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			System.out.println("This user does not follow any accounts.");
+			//e.printStackTrace();
+		}
+
 	}
 
-	
-	public static void main(String [ ] args) throws IOException{
-		getUserID();
-		
-		getUserInfo(username);
-		
-		getUserFriends(username);
-		
-		
-		
+	public static void main(String[] args) throws IOException {
+		while (true) {
+
+			getUserID();
+
+			getUserInfo(username);
+
+			getUserFriends(username);
+		}
+
 	}
-
-	
-
 
 	private static void getUserID() {
 		Scanner in = new Scanner(System.in);
 
-		
 		System.out.println("Please Enter Your Last.FM Username: ");
-		
-		
+
 		username = in.nextLine();
-		
-		in.close();
-		
+
+		// in.close();
+
 	}
-	
-	private static InputStream APISend(URL url) throws IOException{
+
+	private static InputStream APISend(URL url) throws IOException {
 
 		final URLConnection urlConnection = url.openConnection();
 		urlConnection.setDoOutput(true);
@@ -156,9 +138,9 @@ public class Main {
 		final OutputStream outputStream = urlConnection.getOutputStream();
 		outputStream.flush();
 		final InputStream inputstream = urlConnection.getInputStream();
-		
+
 		return inputstream;
-		
+
 	}
 
 }
