a1fc61727da8cb753a62909be85d8d998397d805
thomasgreen <tomgreen135@gmail.com>
2017-04-14 13:32:03 +0100

    Commenting

--- RecSys/src/_Default/TopItem.java
+++ RecSys/src/_Default/TopItem.java
@@ -6,7 +6,7 @@ import artistsPOJO.Attr_;
 
 
 public abstract class TopItem<T> {
-
+	//T represents an Item: either Artist or Track
 	public abstract List<T> getItem();
 
 	public abstract Attr_ getAttr();
